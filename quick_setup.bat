df9f87c90110968acc66078fe1a7f50e9506b005
hadron13 <62845905+hadron13@users.noreply.github.com>
2025-04-15 12:17:46 -0300

    Create quick_setup.bat

---
+++ quick_setup.bat
@@ -0,0 +1,5 @@
+curl -L https://github.com/odin-lang/Odin/releases/download/dev-2025-04/odin-windows-amd64-dev-2025-04.zip --ssl-no-revoke > odin.zip
+mkdir odin 
+cd odin 
+tar -xf ../odin.zip
+del ..\odin.zip
