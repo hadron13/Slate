c4dd232d75d8a45bdb2f64968b1244348f73f685
hadron13 <62845905+hadron13@users.noreply.github.com>
2025-04-23 12:47:33 -0300

    Update quick_setup.bat

--- quick_setup.bat
+++ quick_setup.bat
@@ -1,5 +1,4 @@
 curl -L https://github.com/odin-lang/Odin/releases/download/dev-2025-04/odin-windows-amd64-dev-2025-04.zip --ssl-no-revoke > odin.zip
 mkdir odin 
-cd odin 
-tar -xf ../odin.zip
-del ..\odin.zip
+powershell -command "Expand-Archive -Path 'odin.zip' -DestinationPath '.\odin'"
+del odin.zip
