a9dc4433e925eddeadf8195345e667d6d4c9137b
hadron13 <carlosbpfonseca@gmail.com>
2025-08-01 10:55:35 -0300

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 mkdir -p mods/render
+mkdir -p mods/world
 
 if [[ render/render.odin -nt mods/render/render.so ]]; then
-    odin build render -out:mods/render/render.so -build-mode:shared -debug
+    odin build render -out:mods/render/render.so -build-mode:shared -o:speed
+fi
+if [[ world/world.odin -nt mods/world/world.so ]]; then
+    odin build world -out:mods/world/world.so -build-mode:shared -debug
 fi
 if [[ slate/slate.odin -nt slate.bin ]]; then
     odin build slate -out:slate.bin -debug
