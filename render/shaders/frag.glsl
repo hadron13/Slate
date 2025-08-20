3898d6a017b30c8f95e84a74656e481c4638e641
aluno <carlosbpfonseca@gmail.com>
2025-08-20 13:08:40 -0300

    Prepare for AO implementation

--- render/shaders/frag.glsl
+++ render/shaders/frag.glsl
@@ -1,9 +1,10 @@
 #version 330
 
 in vec3 texCoord;
+in float ambientOcclusion;
 
 uniform sampler2DArray tex;
 
 void main(){
-    gl_FragColor = texture(tex, texCoord);
+    gl_FragColor = texture(tex, texCoord) - vec4(vec3(ambientOcclusion), 0.0);
 }
